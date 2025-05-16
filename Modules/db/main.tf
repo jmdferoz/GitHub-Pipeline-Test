beb5c2dd0f8f8c39d562a208e3dbf4eee167aa73
jmdferoz <55978134+jmdferoz@users.noreply.github.com>
2025-05-16 12:57:40 +0530

    Update main.tf

--- Modules/db/main.tf
+++ Modules/db/main.tf
@@ -1,16 +1,14 @@
 resource "azurerm_mssql_server" "sql_server" {
-  name                         = var.sql_server_name
+  name                         = "sqlserverdemo123"
   resource_group_name          = var.resource_group_name
   location                     = var.location
   version                      = "12.0"
-  administrator_login          = var.sql_admin_username
-  administrator_login_password = var.sql_admin_password
+  administrator_login          = var.sql_admin_user
+  administrator_login_password = var.sql_password
 }
 
 resource "azurerm_mssql_database" "sql_db" {
-  name                = var.sql_db_name
-  resource_group_name = var.resource_group_name
-  location            = var.location
-  server_name         = azurerm_sql_server.sql_server.name
-  sku_name            = "Basic"
+  name      = "demodatabase"
+  server_id = azurerm_mssql_server.sql_server.id  
+  sku_name  = "Basic"
 }
