cff38a622f1312922675b56c2ffbe7664f02ece1
jmdferoz <55978134+jmdferoz@users.noreply.github.com>
2025-05-16 13:01:32 +0530

    Update main.tf

--- Modules/db/main.tf
+++ Modules/db/main.tf
@@ -3,8 +3,8 @@ resource "azurerm_mssql_server" "sql_server" {
   resource_group_name          = var.resource_group_name
   location                     = var.location
   version                      = "12.0"
-  administrator_login          = var.sql_admin_user
-  administrator_login_password = var.sql_password
+  administrator_login          = var.sql_admin_username
+  administrator_login_password = var.sql_admin_password
 }
 
 resource "azurerm_mssql_database" "sql_db" {
